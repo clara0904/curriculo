b3bcbbf063e4149ca0e2375ca8245612c3a64993
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-08-16 21:45:50 -0300

    feat: criação da NavBar

---
+++ src/sections/NavBar/NavBar.jsx
@@ -0,0 +1,27 @@
+﻿import styles from './NavBar.module.css'
+import { BsLinkedin } from "react-icons/bs";
+import { IoLogoGithub } from "react-icons/io5";
+
+function NavBar(){
+    return(
+        <div className={styles.fundo}>
+            <div className={styles.container}>
+                <div className={styles.title}>
+                    Maria Clara Araújo
+                </div>
+                <div className={styles.icons}>
+                    <a href='https://www.linkedin.com/in/maria-clara-ara%C3%BAjo-089305220/' target="_blank" rel="noopener noreferrer">
+                        <BsLinkedin className={styles.icon} size={25} color='#FFF'/>
+                    </a>
+                    
+                    <a href='https://github.com/clara0904' target="_blank" rel="noopener noreferrer">
+                        <IoLogoGithub className={styles.icon} size={27} color='#FFF'/>
+                    </a>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
