83fdc20f247012a4efc7c3e52d47af8707de8826
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-08-19 21:40:22 -0300

    feat: criação da seção Welcome

---
+++ src/sections/Welcome/Welcome.jsx
@@ -0,0 +1,21 @@
+﻿import styles from './Welcome.module.css';
+import Profile from '../../assets/profile.jpg';
+
+function Welcome(){
+    return(
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <div className={styles.text}>
+                    <h1>Olá, eu sou a Maria Clara</h1>
+                    <h4>Técnica em informática e graduanda
+                        em Ciência da Computação
+                    </h4>
+                </div>
+
+                <img src={Profile} alt='Profile' className={styles.profile}/>
+            </div>
+        </div>
+    )
+}
+
+export default Welcome;
