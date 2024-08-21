d3d9a05481a04d14326fb84b9ca34156938576ca
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-08-21 15:37:08 -0300

    feat: configurações gerais

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import NavBar from './sections/NavBar/NavBar';
+import Welcome from './sections/Welcome/Welcome';
+import About from './sections/About/About';
+import Certifications from './sections/Certifications/Certifications';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
+import Footer from './sections/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <NavBar/>
+      <Welcome/>
+      <About/>
+      <Certifications/>
+      <Projects/>
+      <Skills/>
+      <Footer/>
     </div>
   );
 }
