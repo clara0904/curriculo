87db053acc3131727f0283b058357d617bfe7710
Maria Clara <mclaradearaujop@aluno.uespi.br>
2024-08-21 15:34:00 -0300

    feat: criação do component TextCard

---
+++ src/components/Card/TextCard/TextCard.jsx
@@ -0,0 +1,20 @@
+﻿import styles from './TextCard.module.css';
+import { checkPropTypes } from 'prop-types';
+
+TextCard.propTypes = {
+    title: checkPropTypes.string,
+    description: checkPropTypes.string,
+    college: checkPropTypes.string,
+};
+
+function TextCard({title, description, college}){
+    return(
+        <div className={styles.card}>
+            <p className={styles.cardTitle}>{title}</p>
+            <p className={styles.college}>Por: {college}</p>
+            <p className={styles.cardDescription}>{description}</p>
+        </div>
+    )
+}
+
+export default TextCard;
