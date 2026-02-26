9b3cdaca614273605031c810e99c35450ee4b488
clara0904 <mclaradearaujop@aluno.uespi.br>
2026-02-26 14:32:06 -0300

    Ajustes de responsividade e atualizações da seção About

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -8,9 +8,17 @@ function About(){
                     <div>Sobre mim</div>
                 </div>
                 <div className={styles.text}>
-                <p>Sou técnica em informática pelo Instituto Federal do Piauí e atualmente estou cursando Ciência da Computação na Universidade Estadual do Piauí, campus Piripiri. Durante minha formação, adquiri conhecimentos em diversas linguagens de programação, como Python, Pascal, C e Java, além de ter escrito um artigo intitulado <a href={'https://github.com/clara0904/Artigo'} target="_blank" rel="noopener noreferrer"><u>"Algoritmo de predição e personalização da experiência online do usuário"</u></a>. Também possuo experiência em banco de dados, testes unitários, metodologias ágeis e desenvolvimento web utilizando HTML, CSS, JavaScript e TypeScript.</p><br />
-                <p>Minha organização e atenção aos detalhes fazem com que eu me identifique muito com a área de gestão de projetos, na qual adquiri experiência atuando como Scrum Master no projeto Portfólio LES, em que também desempenhei o papel de desenvolvedora frontend, utilizando o framework React.</p><br/>
-                <p>Sou extremamente dedicada aos meus projetos e estudos, sempre em busca de novos conhecimentos, tanto na área de tecnologia quanto em outras áreas do saber, atualmente estou dedicada a estudar as áreas de cibersegurança e redes. Sou uma leitora assídua, apaixonada por filmes, natureza, música e, claro, por tecnologia.</p>
+                <p>
+                Sou técnica em informática pelo Instituto Federal do Piauí e atualmente curso Ciência da Computação na Universidade Estadual do Piauí, campus Piripiri. Ao longo da minha formação, desenvolvi conhecimentos em diversas linguagens de programação, como Python, Pascal, C e Java, além de experiência em banco de dados, testes unitários, metodologias ágeis e inteligência artificial. Também atuei no desenvolvimento web utilizando HTML, CSS, JavaScript e TypeScript, com experiência prática no framework React, e possuo habilidades em desenvolvimento mobile com Flutter.
+                </p>
+                <br />
+                <p>
+                Atualmente, atuo como estagiária no Núcleo de Processamento de Dados da universidade, participando de projetos de auditoria de redes e segurança. Essa experiência me proporcionou vivência prática em ambientes corporativos, fortalecendo minha capacidade de análise, organização e resolução de problemas.
+                </p>
+                <br/>
+                <p>
+                Sou dedicada e comprometida com meus projetos e estudos, sempre em busca de novos conhecimentos, tanto na área de tecnologia quanto em outros campos. Atualmente, concentro meus esforços em aprofundar conhecimentos em cibersegurança e redes. Além disso, sou uma leitora assídua e tenho paixão por filmes, natureza, música e, claro, tecnologia.
+                </p>
                 </div>
             </div>  
         </div>
