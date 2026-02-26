444d21258d44742412333ec5e7e0867c5b7cb300
clara0904 <mclaradearaujop@aluno.uespi.br>
2026-02-26 14:30:24 -0300

    Ajustes e atualizações na seção skills

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -4,6 +4,8 @@ import css from '../../assets/css.png'
 import js from '../../assets/js.png'
 import react from '../../assets/react.png'
 import java from '../../assets/java.png'
+import angular from '../../assets/angular.png'
+import flutter from '../../assets/flutter.png'
 
 function Skills(){
     return(
@@ -12,11 +14,13 @@ function Skills(){
                 <div className={styles.title}>Minhas principais habilidades</div>
 
                 <div className={styles.images}>
-                    <img src={html} alt='HTML' className={styles.image}/>
-                    <img src={css} alt='CSS' className={styles.image}/>
-                    <img src={js} alt='JS' className={styles.image}/>
-                    <img src={react} alt='REACT' className={styles.image}/>
-                    <img src={java} alt='JAVA' className={styles.image}/>
+                    <div className={styles["image-wrapper"]}><img src={html} alt="HTML" /></div>
+                    <div className={styles["image-wrapper"]}><img src={css} alt="CSS" /></div>
+                    <div className={styles["image-wrapper"]}><img src={js} alt="JS" /></div>
+                    <div className={styles["image-wrapper"]}><img src={react} alt="React" /></div>
+                    <div className={styles["image-wrapper"]}><img src={java} alt="Java" /></div>
+                    <div className={styles["image-wrapper"]}><img src={angular} alt="Angular" /></div>
+                    <div className={styles["image-wrapper"]}><img src={flutter} alt="Flutter" /></div>
                 </div>
             </div>
         </div>
